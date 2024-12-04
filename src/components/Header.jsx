9474ff43026bbfb8001d6de76b58f4f4cf825246
Jennifer <jmarthagodfrey@gmail.com>
2024-12-04 09:44:44 -0700

    setup: installed tailwind, confirmed components render

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header>
+            <img src="/pantry.webp" alt="Pantry to Plate Logo" />
+            Pantry To Plate
+        </header>
+    );
+};
+
+export default Header;
