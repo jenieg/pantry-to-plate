a01a327b0ad979cf8e8f964bbbc7c64f3b9f1656
Jennifer <jmarthagodfrey@gmail.com>
2025-01-08 13:47:53 -0700

    feat: api is working

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,19 +4,29 @@ import Recipe from "./Recipe";
 import { getRecipeFromMistral } from "../api/ai"
 
 const Main = () => {
-    //Ingredients
-    const [ingredients, setIngredients] = React.useState(["all the main spices", "pasta", "ground beef", "tomato paste"])
+    //Ingredients State
+    const [ingredients, setIngredients] = React.useState([])
 
+    //Recipe and visibility state
+    const [recipeMarkdown, setRecipeMarkdown] = React.useState("");
     const [recipeShown, setRecipeShown] = React.useState(false)
     
+    //add ingredient function
     function addIngredient(formData) {
         const newIngredient = formData.get("ingredient")
-        
         setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
 
-    function handleClick() {
-        setRecipeShown(prevShown => !prevShown);
+    //button click function
+    async function handleClick() {
+        try {
+          // Fetch the recipe with ingredients stored in state
+            const recipe = await getRecipeFromMistral(ingredients);
+            setRecipeMarkdown(recipe); // Store the markdown response
+            setRecipeShown(true); // Show the Recipe component
+        } catch (error) {
+            console.error("Error fetching recipe:", error);
+        }
     }
 
     return (
@@ -43,7 +53,7 @@ const Main = () => {
                     handleClick={handleClick}
                 />}
 
-            {recipeShown && <Recipe />}
+            {recipeShown && <Recipe markdown={recipeMarkdown}/>}
         </main>
     );
 };
