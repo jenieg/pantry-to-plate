96f511633d2e8ce4f1980aa3d07349df44e11a84
Jennifer <jmarthagodfrey@gmail.com>
2025-01-08 14:18:30 -0700

    feat: tailwind styling markdown to prose

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -5,7 +5,9 @@ import remarkGfm from 'remark-gfm';
 const Recipe = ({ recipe }) => {
     return (
         <section className="max-w-3xl mt-7 px-5 py-5">
-            <ReactMarkdown remarkPlugins={[remarkGfm]}>
+            <ReactMarkdown 
+                remarkPlugins={[remarkGfm]}
+                className="prose prose-md">
                 {recipe}
             </ReactMarkdown>
         </section>
