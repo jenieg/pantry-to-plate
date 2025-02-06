473bd7e0ed4b55c2586c2e0a2bcf0079ee7cc9b0
Jennifer <jmarthagodfrey@gmail.com>
2025-02-06 10:05:33 -0700

    fix: update prompt

--- netlify/functions/generateRecipe.js
+++ netlify/functions/generateRecipe.js
@@ -2,7 +2,7 @@
 const { HfInference } = require("@huggingface/inference");
 
 const SYSTEM_PROMPT = `
-You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page. If someone asks for anything that is not a recipe say "I'm sorry, I can only make recipes.
+You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page. If someone asks for anything that is not a recipe reply with "I'm sorry, I can only make recipes", otherwise do the above.
 `;
 
 exports.handler = async (event) => {
